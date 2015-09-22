87a981513cd55b5ee04a72453aaefbda441f3b63
IJonez <db2442s@FB6PC00023.fb6.fh-aachen.de>
2015-09-22 16:04:02 +0200

    no need to sanitize inputs_0.01

--- postprocessing/zwokompdez.py
+++ postprocessing/zwokompdez.py
@@ -32,28 +32,31 @@ def zwokompdez(a):
             return(a)
 
 fobj_in = open('Messwerte_roh.txt')
-fobj_out = open('Messwerte_dez' + 'txt',"w")
+fobj_out_m = open('Messwerte_m' + 'txt',"w")
+fobj_out_dez = open('Messwerte_dez' + 'txt',"w")
 i = 1
+
 for line in fobj_in:
     #print('for_schleife')
                 
-    if i > 3:
-        if len(line) == 0:
-            fobj_in.close()
-            fobj_out.close()
-            print('ende')
-        #print('ifverzweigung')
-        line = str(line)
-        ausgabe = str(zwokompdez(line))
-        Moment = (float(ausgabe) / 8388608) *4.27*10**(-10)* 70*10**9*0.06/0.07/0.004
-        print(str(ausgabe))
-        fobj_out.write(str(ausgabe) + str(Moment) + '\n')
-    i += 1
+    if n > 3:
+	
+        if len(line) >= 1:
+		  	#print('ifverzweigung')
+			line = str(line)
+			ausgabe = str(zwokompdez(line))
+			Moment = (float(ausgabe) / 8388608) *4.27*10**(-10)* 70*10**9*0.06/0.07/0.004)
+			
+			print(str(ausgabe))
+			fobj_out_dez.write(str(ausgabe) +'\n')
+			fobj_out_m.write(str(Moment) + '\n')		
+    n += 1
 
 
 
 fobj_in.close()
-fobj_out.close()
+fobj_out_dez.close()
+fobj_out_m.close()
 system('gnuplot -persist test_daten.gp')
 input('ende')
 
